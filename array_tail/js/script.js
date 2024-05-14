69b649924daeb7f0d3d83758140bb5f7c2bbd4eb
AlesciDavide <davi.alesci@email.com>
2024-05-14 14:44:23 +0200

    creo ciclo for per inserire i numeri nell'array e stampare gli ultimi 5

--- array_tail/js/script.js
+++ array_tail/js/script.js
@@ -4,3 +4,10 @@ const lista = [];
 if(numeroUtente > 50 || numeroUtente < 0 || Number.isNaN(numeroUtente)){
     numeroUtente = 20;
 }
+
+for(let i= 0; i < numeroUtente; i++){
+    lista.push(Math.floor(Math.random() * 100 + 1));
+}
+
+console.log(lista.slice(numeroUtente - 5));
+
