579ef7db062703f0e91d8e032d6853bb5a74652c
AlesciDavide <davi.alesci@email.com>
2024-05-14 14:32:01 +0200

    creo il ciclo for e l'if per controllare se 'cocomero' è presente

--- array-lista/js/script.js
+++ array-lista/js/script.js
@@ -3,3 +3,12 @@ listaSpesa.push('cocomero');
 
 
 let checkValue = 0;
+
+for(let i = 0; i < listaSpesa.length; i++ ){
+
+    if(listaSpesa[i] == 'cocomero'){
+        checkValue = 1;
+    }
+    
+}
+
