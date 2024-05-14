95391030de760ea0ac22a10e2f5d99d3afca0ea7
AlesciDavide <davi.alesci@email.com>
2024-05-14 14:38:27 +0200

    faccio il controllo per i numeri inseriti dall'utente

--- array_tail/js/script.js
+++ array_tail/js/script.js
@@ -1,3 +1,6 @@
 let numeroUtente = Number.parseInt(prompt('Quanti elementi vuoi inserire nel nostro array?'));
 const lista = [];
 
+if(numeroUtente > 50 || numeroUtente < 0 || Number.isNaN(numeroUtente)){
+    numeroUtente = 20;
+}
