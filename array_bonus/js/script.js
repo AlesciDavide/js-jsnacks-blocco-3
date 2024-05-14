1e219695f5f39b29634d5805f28c05db1af2d791
AlesciDavide <davi.alesci@email.com>
2024-05-14 15:03:48 +0200

    aggiunto l'if per il controllo e inserisco nell'array i numeri mancanti

--- array_bonus/js/script.js
+++ array_bonus/js/script.js
@@ -1,3 +1,10 @@
-const listaUno = [5,10,15,60,4,3,8,6,5,];
-const listaDue = [3,8,6,5,1,2,5];
-let check = 0;
+const listaUno = [5,10,15,60,4,3,8,6,5,5,2,6,45,5,6];
+const listaDue = [3,8,6,5,1,2,5,5,10,15,60,4,3,8,6,5,5,2,6,45,5,6];
+let x;
+
+if(listaUno.length > listaDue.length){
+    x = listaUno.length - listaDue.length;
+    for(let i = 0; i < x; i++){
+        listaDue.push(Math.floor(Math.random()* 100 + 1));
+    }
+}
