0b5706933b8670ab6a8cb0e16ea3d949e0835e20
AlesciDavide <davi.alesci@email.com>
2024-05-15 12:45:53 +0200

    snack 11

--- snack10/js/script.js
+++ snack10/js/script.js
@@ -8,11 +8,23 @@ const listaDue = [3,8,6,5,1,2,5,5,10,15,60,4,3,8,6,5,5,2,6,45,5,6];
 if(listaUno.length > listaDue.length){
     while(listaUno.length != listaDue.length){
         listaDue.push(Math.floor(Math.random()* 100 + 1));
-        console.log(listaUno, listaDue)
     }
-    }else{
-        while(listaUno.length != listaDue.length){
-        listaUno.push(Math.floor(Math.random()* 100 + 1));
+}else{
+    while(listaUno.length != listaDue.length){
+    listaUno.push(Math.floor(Math.random()* 100 + 1));
 }
 }
+console.log(listaUno, listaDue);
+
+
+/* senza if */
+
+
+while(listaUno.length < listaDue.length){
+    listaUno.push(Math.floor(Math.random()* 100 + 1));
+}
+while(listaUno.length > listaDue.length){
+    listaDue.push(Math.floor(Math.random()* 100 + 1));
+}
+
 console.log(listaUno, listaDue);
