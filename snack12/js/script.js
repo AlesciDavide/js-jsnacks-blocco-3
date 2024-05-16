c468dfcc8faf7d0e307699a421d7fd1b2b797254
AlesciDavide <davi.alesci@email.com>
2024-05-16 11:44:57 +0200

    sneck 12

---
+++ snack12/js/script.js
@@ -0,0 +1,19 @@
+// Snack12
+// Dare la possibilità di inserire due parole. Verificare tramite una funzione che le due parole abbiano la stessa lunghezza. Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
+
+
+
+function lunghezaParole(parolaUno, parolaDue){
+
+    if(parolaUno.length === parolaDue.length){
+        console.log(parolaUno, parolaDue);
+    } else if (parolaUno.length > parolaDue.length){
+        console.log(parolaUno);
+    }else{
+        console.log(parolaDue);
+    }
+}
+
+let testoUno = 'ciao';
+let testoDue = 'ciao';
+lunghezaParole(testoUno, testoDue);
